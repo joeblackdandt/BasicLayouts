437a965acd33f5c1c5b3a5f7604a5957e1cfdca6
Mac <mac@Macs-iMac.local>
2018-12-09 12:45:57 -0500

    First Commit

--- BasicLayouts/ViewController.swift
+++ BasicLayouts/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var myLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
+    @IBAction func didTapButton(_ sender: Any) {
+        let name = myTextField.text!
+        
+        myLabel.text = "Hi \(name)!"
+        
+        myTextField.resignFirstResponder()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
